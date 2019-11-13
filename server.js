d41443e6f109d3e8dd9fbd0333d8860336ce277d
laywing04 <yuyu04@inka.co.kr>
2019-11-13 16:36:08 +0900

    change

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var createServer = require("auto-sni");
 var express = require('express');
 var fs = require('fs');
-// var https = require('https');
+var http = require('http');
+//var https = require('https');
 
 // var privateKey = fs.readFileSync('ssl/private.key', 'utf8');
 // var certificate = fs.readFileSync('ssl/private.pem', 'utf8');
 
-var credentials = {key: privateKey, cert: certificate};
+// var credentials = {key: privateKey, cert: certificate};
 var app = express();
 
 const bodyParser = require('body-parser');
@@ -21,11 +22,12 @@ app.engine('html', require('ejs').renderFile);
 app.use(express.static('www'));
 //app.use('/', router);
 
-createServer({ email: "yuyu04@naver.com", domains: "www.jing-jing.cf", agreeTos: true, ports:{http:80, https: 443} }, app);
+var httpServer = http.createServer(app);
+createServer({ email: 'yuyu04@naver.com', domains: ['jing-jing.cf', 'www.jing-jing.cf'], agreeTos: true, ports: {https: 443} }, app);
+// var httpsServer = https.createServer(credentials, app);
 
-//var httpsServer = https.createServer(credentials, app);
-
-//httpsServer.listen(8080, '172.31.38.77');
+httpServer.listen(80, '172.31.38.77');
+// httpsServer.listen(8080, '172.31.38.77');
 
 // var server = app.listen(3000, function() {
 //     console.log("Express server has started on port 3000")
