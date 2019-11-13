4c1bbbca91e03c22b590cbce81465a9a10ba0b7c
laywing04 <yuyu04@inka.co.kr>
2019-11-14 08:10:05 +0900

    change

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var createServer = require("auto-sni");
 var express = require('express');
 var fs = require('fs');
 var http = require('http');
+var cookieParser = require('cookie-parser');
 //var https = require('https');
 
 // var privateKey = fs.readFileSync('ssl/private.key', 'utf8');
@@ -12,6 +13,8 @@ var app = express();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
 
 var router = require('./router/main')(app);
 
@@ -20,13 +23,27 @@ app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
 app.use(express.static('www'));
-//app.use('/', router);
+
+app.use(function(req, res, next) {
+   var err = new Error('Not Found');
+   err.status = 404;
+   next(err);
+});
+
+app.use(function(err, req, res, next) {
+   res.status(err.status || 500);
+   res.render('error', {
+      message: err.message,
+      error: {}
+   });
+});
 
 var httpServer = http.createServer(app);
+httpServer.listen(80, '3.134.113.218');
+
 createServer({ email: 'yuyu04@naver.com', domains: ['jing-jing.cf', 'www.jing-jing.cf'], agreeTos: true, ports: {https: 443} }, app);
 // var httpsServer = https.createServer(credentials, app);
 
-httpServer.listen(80, '3.134.113.218');
 // httpsServer.listen(8080, '172.31.38.77');
 
 // var server = app.listen(3000, function() {
