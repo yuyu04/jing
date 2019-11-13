8321034e4c7652de3412a5fad0311e587e340572
laywing04 <yuyu04@inka.co.kr>
2019-11-13 12:09:28 +0900

    upload

--- router/main.js
+++ router/main.js
@@ -2,32 +2,146 @@ module.exports = function(app)
 {
     // Web push
     const webpush = require('web-push');
-// VAPID keys should only be generated only once.
+
+    // VAPID keys should only be generated only once.
     const vapidKeys = webpush.generateVAPIDKeys();
+
+    const publicKey = 'BEdMy4CXnj2VLTCrKKBPZfgIaURGcfmOPsjh1BBDnv-72Uj9WsSnbU8ce-vogrJwKxpiedEiYjrrkTjaHjHQa-A';
+    const privateKey = 'VpSgjyWs6Q27voDmefD4xZJYdYjyloubHiaFERJUv44';
 
     webpush.setGCMAPIKey('AAAASh6YDxE:APA91bEwr2UlW981dPTMQXSUvzQXh1yiklqFFCTmi0BlH0ue9aILC4NHjmpaUlQ-ZjFfH9hVoi0Ung6_GM6ZqioMidNzFju_chCrx6E8LgCBTlJk-tyMNssJJow1NR7Jooc4bu1J56_h');
     webpush.setVapidDetails(
-        'mailto:your E-Mail',
-        vapidKeys.publicKey,
-        vapidKeys.privateKey
+        'mailto:yuyu04@inka.co.kr',
+        publicKey,
+        privateKey
     );
+    console.log("server start", vapidKeys.publicKey);
+    console.log("server start2", vapidKeys.privateKey);
+    const MongoClient = require('mongodb').MongoClient;
+    const uri = "mongodb+srv://yuyu04:85tmffkdla@cluster0-ycxrb.mongodb.net/test?retryWrites=true&w=majority";
+
+    let countId = 0;
 
     app.get('/',function(req,res){
         res.render('index.html')
     });
 
-    app.post('/push-send', function(request, reply) {
-        const subscription = JSON.parse(request.payload.subscription);
-        const data = request.payload.data;
+    app.get('/get-db', function(req, res) {
+        let responseData = null;
+        const client = new MongoClient(uri, { useNewUrlParser: true });
+        client.connect(err => {
+            if (err) {
+                console.error('MongoDB 연결 실패', err);
+                res.status(500).send(err);
+                return;
+            }
+
+            const collection = client.db("jingDb").collection("customers");
+            // perform actions on the collection object
+
+            collection.find().toArray(function (err, docs) {
+                client.close();
+                if (err) {
+                    console.error('find err', err);
+                    res.status(500).send(err);
+                    return;
+                }
+                res.send(docs);
+            });
+        });
+    });
+
+    app.post('/remove-all', function(req, res) {
+        const isRemove = req.body.isRemove;
+        if (isRemove == null) {
+            res.status(500).send('dont remove item');
+        }
+
+        const client = new MongoClient(uri, { useNewUrlParser: true });
+        client.connect(err => {
+            console.log("connection");
+            if (err) {
+                console.error('MongoDB 연결 실패', err);
+                res.status(500).send(err);
+                return;
+            }
+
+            const collection = client.db("jingDb").collection("customers");
+
+            collection.removeMany({});
+            client.close();
+        });
+    });
+
+    app.post('/push-send', function(req, res) {
+        const id = Number(req.body.id);
+        if (id == null) {
+            res.status(500).send('id is Null');
+        }
+
+        const data = req.body.data;
         const options = {
             TTL: 24 * 60 * 60,
             vapidDetails: {
-                subject: 'mailto: UR E-mail',
-                publicKey: vapidKeys.publicKey,
-                privateKey: vapidKeys.privateKey
+                subject: 'mailto:yuyu04@inka.co.kr',
+                publicKey: publicKey,
+                privateKey: privateKey
             }
         };
-        webpush.sendNotification( subscription, data, options );
-        return reply({statusCode:200, data : "OK"}).code(200);
+        console.log(id, data);
+
+        const client = new MongoClient(uri, { useNewUrlParser: true });
+        client.connect(err => {
+            console.log("connection");
+            if (err) {
+                console.error('MongoDB 연결 실패', err);
+                res.status(500).send(err);
+                return;
+            }
+
+            const collection = client.db("jingDb").collection("customers");
+
+            collection.findOne({ ID: id })
+                .then(function(result) {
+                    let subscription = JSON.parse(result.subscription);
+                    console.log(subscription);
+                    webpush.sendNotification(subscription, data, options).catch(error => {
+                        console.log(error);
+                    });
+                    client.close();
+                })
+                .catch(function(error) {
+                    console.log(error);
+                    client.close();
+                });
+        });
+
+        res.send('POST request to the homepage');
+    });
+
+    app.post('/save-subs', function(request, reply) {
+        const subscription = request.body.subscription;
+        if (subscription == null) {
+            reply.status(500).send('subscription is Null');
+        }
+
+        const client = new MongoClient(uri, { useNewUrlParser: true });
+        client.connect(err => {
+            if (err) {
+                console.error('MongoDB 연결 실패', err);
+                reply.status(500).send(err);
+                return;
+            }
+
+            const collection = client.db("jingDb").collection("customers");
+            // perform actions on the collection object
+
+            collection.insertOne({ID: countId, subscription: subscription});
+            countId += 1;
+            client.close();
+        });
+
+        reply.send('POST request to the homepage');
+        //return reply({statusCode:200, data : "OK"}).code(200);
     });
 }
--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 var express = require('express');
+var fs = require('fs');
+var https = require('https');
+
+var privateKey = fs.readFileSync('ssl/private.key', 'utf8');
+var certificate = fs.readFileSync('ssl/private.pem', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
 var app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 var router = require('./router/main')(app);
 
 app.set('views', __dirname + '/www');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
-app.use(express.static('www'));
 
-var server = app.listen(3000, function(){
-    console.log("Express server has started on port 3000")
-});
+app.use(express.static('www'));
+//app.use('/', router);
+
+var httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(8080, '172.31.38.77');
+
+// var server = app.listen(3000, function() {
+//     console.log("Express server has started on port 3000")
+// });
 
 
